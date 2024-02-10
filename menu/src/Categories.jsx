0e5e077aef573d7917a4a457db88b8051eadb6a4
Erkin Dilekci <dilekcierkin@gmail.com>
2024-02-10 10:11:09 +0300

    Update Categories.jsx

--- menu/src/Categories.jsx
+++ menu/src/Categories.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import menu from "./data.js";
 
 const Categories = ({handleSelect}) => {
-    const categories = [...new Set(menu.map(m => m.category))];
+    const categories = ['all', ...new Set(menu.map(m => m.category))];
 
     return (
         <div className="btn-container">
-            <button onClick={() => handleSelect('all')} className="filter-btn">All</button>
             {categories.map(c => <button onClick={() => handleSelect(c)} className="filter-btn" key={c}>{c}</button>)}
         </div>
     );
